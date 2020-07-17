3f8f7e6d23aeb9a65c9833a9e24ab3bfccde9895
rabpeter <de.nimitz@yandex.com>
2020-07-17 09:23:20 -0500

    feat: setup backend server

---
+++ server/src/server.js
@@ -0,0 +1,93 @@
+//require the express module
+import express from "express"
+
+const bodyParser = require("body-parser");
+import { sendMessages } from './socket/chat'
+import api from './api/index'
+
+const app = express();
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const config = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+const port = normalizePort(process.env.PORT || '5001')
+
+app.use(config)
+app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(api)
+
+
+
+server.listen(port, onListening)
+server.on('error', onError)
+server.on('listening', onListening)
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    // named pipe
+    return val
+  }
+
+  if (port >= 0) {
+    // port number
+    return port
+  }
+
+  return false
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string'
+    ? `Pipe ${port}`
+    : `Port ${port}`
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`)
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`)
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address()
+  const bind = typeof addr === 'string'
+    ? `pipe ${addr}`
+    : `port ${addr.port}`
+  console.log('Listening on ', bind)
+}
