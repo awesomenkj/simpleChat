e36596f3dec3e8e53a5edb14f5e5c748cb3d312e
rabpeter <de.nimitz@yandex.com>
2020-07-20 19:26:53 -0500

    feat: updated api

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -6,7 +6,31 @@ import User from '../db/models/User'
 
 const route = express.Router()
 
-
+route.get('/v1/rooms/:roomId/messages', (req, res) => {
+  const roomId = req.params.roomId;
+  const username = req.query.username;
+  console.log('roomId: ', roomId)
+  console.log('req.query: ', req.query)
+  Room.find({ _id: roomId }, (err, rooms) => {
+    if(err) return res.status(500).send({ success: false, error: err.toString()})
+    console.log('rooms: ', rooms)
+    const roomUsers = rooms[0].users
+    if (roomUsers.includes(req.query.username)) {
+      Message.find({ roomId }, (err, messages) => {
+        if(err) return res.status(500).send({ success: false, error: err.toString()})
+        return res.status(200).send({
+          success: true,
+          data: messages
+        })
+      })
+    } else {
+      return res.status(200).send({
+        success: false,
+        error: 'Access denied'
+      })
+    }
+  })
+})
 
 route.post('/v1/rooms/create', (req, res) => {
   const username = req.body.username;
@@ -34,6 +58,34 @@ route.post('/v1/rooms/create', (req, res) => {
   })
 })
 
+route.post('/v1/rooms/join', (req, res) => {
+  const username = req.body.username;
+  const roomId = req.body.roomId;
+  console.log('Join roomId: ', roomId)
+  console.log('Join username: ', username)
+  Room.find({ _id: roomId }, (err, rooms) => {
+    if (err) return res.status(500).send({ success: false, error: err.toString() })
+    const roomUsers = rooms[0].users
+    if (roomUsers.includes(username)) {
+      return res.status(200).send({
+        success: true,
+        data: rooms
+      })
+    } else {
+      Room.findByIdAndUpdate(roomId, {$addToSet: { users: username }}, function (err, updated) {
+        if (err) return res.status(500).send({ success: false, error: err.toString() })
+        // updated!
+        console.log('updated: ', updated)
+        return res.status(200).send({
+          success: true,
+          data: updated
+        })
+      })
+    }
+  })
+})
+
+route.delete('/v1/rooms/:id', TODO)
 
 
 function TODO(req, res) {
